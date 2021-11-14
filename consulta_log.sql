77a4de8953acfb758a9d999d7a45b8ed0ec67ff0
Alexandre Nascimento, Jr <35277225+alexandrenjr@users.noreply.github.com>
2021-11-14 15:43:18 -0400

    Now done

--- consulta_log.sql
+++ consulta_log.sql
@@ -1,33 +1,36 @@
+DROP TABLE IF EXISTS consulta_log;
 
-create table consulta_log(
-	old_cod_consulta integer,
-	new_cod_consulta integer,
-	old_data date,
-	new_data date,
-	old_hora time,
-	new_hora time,
-	old_local varchar(50),
-	new_local varchar(50),
-	cns_paciente varchar(15),
-	tipo_alteracao varchar(6),
-	data_alteracao timestamp with time zone
+CREATE TABLE consulta_log(
+	old_cod_consulta INTEGER,
+	new_cod_consulta INTEGER,
+	old_data DATE,
+	new_data DATE,
+	old_hora TIME,
+	new_hora TIME,
+	old_local VARCHAR(50),
+	new_local VARCHAR(50),
+	old_especialidade VARCHAR(50),
+	new_especialidade VARCHAR(50),
+	cns_paciente VARCHAR(15),
+	tipo_alteracao VARCHAR(6),
+	data_alteracao TIMESTAMP WITH TIME ZONE
 );
 
-create or replace function consultalog() returns trigger as $$
-begin
+CREATE OR REPLACE FUNCTION consultalog() RETURNS TRIGGER AS $$
+BEGIN
 	IF TG_OP = 'UPDATE' THEN
-	insert into consulta_log values (old.cod_consulta, new.cod_consulta, old.data, new.data, 
-									 old.hora, new.hora, old.local, new.local, old.cns_paciente, 
-									 'UPDATE', now());
+	INSERT INTO consulta_log VALUES (old.cod_consulta, new.cod_consulta, old.data, new.data, 
+									 old.hora, new.hora, old.local, new.local, old.especialidade,
+									 new.especialidade, old.cns_paciente, 'UPDATE', NOW());
 	ELSE
-	insert into consulta_log values (old.cod_consulta, new.cod_consulta, old.data, new.data, 
-									 old.hora, new.hora, old.local, new.local, old.cns_paciente, 
-									 'DELETE', now());
+	INSERT INTO consulta_log VALUES (old.cod_consulta, new.cod_consulta, old.data, new.data, 
+									 old.hora, new.hora, old.local, new.local, old.cns_paciente,
+									 old.especialidade, new.especialidade 'DELETE', NOW());
 	END IF;
-	return new;
-end;
-$$ language plpgsql;
+	RETURN new;
+END;
+$$ LANGUAGE plpgsql;
 
-create trigger altera_consulta 
-after delete or update of local on consulta
-for each row execute function consultalog();
+CREATE OR REPLACE TRIGGER altera_consulta 
+AFTER DELETE OR UPDATE ON consulta
+FOR EACH ROW EXECUTE FUNCTION consultalog();
